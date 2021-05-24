687727d576587159fa2f87160059ec5acb95ca33
Artem <artemosadach@gmail.com>
2021-05-24 20:11:43 +0300

    Adding search in posts

--- src/components/Posts/MyPosts/MyPosts.jsx
+++ src/components/Posts/MyPosts/MyPosts.jsx
@@ -1,11 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import p from "./MyPosts.module.css"
 import Post from "./Post/Post"
 import PostCreationContainer from "./PostCreation/PostCreationContainer"
 import CreatePost from "./../../../assets/images/editWhite.png"
 import CreatePostLight from "./../../../assets/images/edit.png"
+import Search from "./../../../assets/images/searchW.png"
 
 const MyPosts = props => {
+  const [focus, setFocus] = useState(false)
+
+  let toggleFocus = () => {
+    setFocus(!focus)
+  }
+
   let postElements = props.posts.map(post => (
     <Post
       border={true}
@@ -28,10 +35,43 @@ const MyPosts = props => {
     <>
       {!props.isPostCreation ? (
         <div className={p.PostsBlock}>
-          <header className={p.header}>
-            <div className={p.heading}>
+          <header className={p.header + " " + (focus ? p.active : "")}>
+            <div className={p.heading + " " + (focus ? p.action : "")}>
               <span>Posts</span>
             </div>
+            <div
+              className={p.wrap}
+              // onClick={() => {
+              //   setFocus(!focus)
+              // }}
+            >
+              <form className={p.forma} action='' autocomplete='on'>
+                <input
+                  id='search'
+                  name='search'
+                  type='text'
+                  placeholder='Post search'
+                  onFocus={() => {
+                    setFocus(!focus)
+                  }}
+                  onBlur={() => {
+                    setFocus(!focus)
+                  }}
+                />
+                <img
+                  src={Search}
+                  className={p.searchSubmit}
+                  value='Rechercher'
+                  type='submit'
+                />
+              </form>
+            </div>
+            {/* <form className={p.searchBlock}>
+              <input type="search">
+              <input type='search' />
+              <img className={p.fa} src={Search} alt='search' />
+            </form> */}
+
             <div className={p.editBlock}>
               <img
                 className={p.edit}
