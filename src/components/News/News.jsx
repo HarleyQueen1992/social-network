57cef9e90bcc76c63293cd69e14aa1b1d00b0dfc
Artem <artemosadach@gmail.com>
2021-07-31 23:06:16 +0300

    Added post creation to news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,16 +1,80 @@
-import React, { useState } from "react"
-import s from "./News.module.css"
-import Notification from "./../../assets/images/notification.png"
-import NotificationB from "./../../assets/images/notificationB.png"
-import ProfilePhoto from "./../../assets/images/user.png"
-import Post from "./../Posts/MyPosts/Post/Post"
+import React, { useState, useEffect } from "react";
+import s from "./News.module.css";
+import Notification from "./../../assets/images/notification.png";
+import NotificationB from "./../../assets/images/notificationB.png";
+import ProfilePhoto from "./../../assets/images/user.png";
+import Post from "./../Posts/MyPosts/Post/Post";
+import Gallery from "./../../assets/images/gallery.png";
 
-const News = props => {
-  const [focus, setFocus] = useState(false)
-  let newUrl = window.location.href
+const News = (props) => {
+  const [focus, setFocus] = useState(false);
+  const [isCreatePost, setIsCreatePost] = useState(false);
+  const [valuePostText, setValuePostText] = useState("");
+  const [valuePostTitle, setValuePostTitle] = useState("");
+  let newUrl = window.location.href;
   if (props.strUrlPrev != newUrl) {
-    props.changeIndex(newUrl)
+    props.changeIndex(newUrl);
   }
+  const openPopup = () => {
+    document.querySelector(".react-swipeable-view-container").style.cssText =
+      "transform: translate(50) !important;" + "will-change: auto !important;";
+    setIsCreatePost(true);
+  };
+  const closePopup = () => {
+    setIsCreatePost(false);
+    setValuePostText("");
+    setValuePostTitle("");
+  };
+  const submitPost = () => {
+    props.addPostActionCreator(valuePostText, valuePostTitle);
+    setIsCreatePost(false);
+    setValuePostText("");
+    setValuePostTitle("");
+  };
+  const handleChangePostText = (event) => {
+    setValuePostText(event.target.value);
+  };
+  const handleChangePostTitle = (event) => {
+    setValuePostTitle(event.target.value);
+  };
+  useEffect(() => {
+    var tx = document.getElementById("textarea");
+    let height = tx.style.height;
+    let heightNumber = Number(height.substring(0, height.length - 2));
+    if (heightNumber > 500) {
+      tx.setAttribute("style", "height: 500px;" + "overflow-y:scroll;");
+    } else {
+      tx.setAttribute(
+        "style",
+        "height:" + tx.scrollHeight + "px;overflow-y:hidden;"
+      );
+      tx.addEventListener("input", OnInput, false);
+
+      function OnInput(e) {
+        this.style.height = "auto";
+        this.style.height = this.scrollHeight + "px";
+      }
+    }
+  }, [valuePostText]);
+  useEffect(() => {
+    var tx = document.getElementById("textareaTitle");
+    let height = tx.style.height;
+    let heightNumber = Number(height.substring(0, height.length - 2));
+    if (heightNumber > 60) {
+      tx.setAttribute("style", "height: 60px;" + "overflow-y:scroll;");
+    } else {
+      tx.setAttribute(
+        "style",
+        "height:" + tx.scrollHeight + "px;overflow-y:hidden;"
+      );
+      tx.addEventListener("input", OnInput, false);
+
+      function OnInput(e) {
+        this.style.height = "auto";
+        this.style.height = this.scrollHeight + "px";
+      }
+    }
+  }, [valuePostTitle]);
 
   return (
     <div className={s.newsListPage}>
@@ -19,15 +83,71 @@ const News = props => {
           <img
             className={s.anythingNewProfilePhoto}
             src={ProfilePhoto}
-            alt='profile photo'
+            alt="profile photo"
           />
         </div>
-        <div className={s.anythingNew}>
+        <div className={s.anythingNew} onClick={openPopup}>
           <span>Anything new?</span>
         </div>
       </div>
+      <div
+        className={s.popupCreatePost + " " + (isCreatePost && s.active)}
+        onMouseDown={closePopup}
+      >
+        <div
+          className={s.popupContent}
+          onMouseDown={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <div className={s.popupContentHader}>
+            <div className={s.popupContentHeaderTitle}>Create post</div>
+            <div className={s.popupContentHeaderOff} onClick={closePopup}></div>
+          </div>
+          <div className={s.popupContentBody}>
+            <div className={s.popupContentBodyAuthor}></div>
+            <textarea
+              className={s.popupContentTitleTextarea}
+              id="textareaTitle"
+              value={valuePostTitle}
+              placeholder="Title post!"
+              onChange={handleChangePostTitle}
+            ></textarea>
+            <textarea
+              value={valuePostText}
+              id="textarea"
+              placeholder="What's on your mind?"
+              className={s.popupContentBodyTextarea}
+              onChange={handleChangePostText}
+            >
+              <span></span>
+            </textarea>
+          </div>
+          <div className={s.popupContentAddToYourPostBlock}>
+            <span className={s.popupContentAddToYourPost}>
+              Add to your post
+            </span>
+            <img
+              className={s.popupContentAddToYourPostGallery}
+              src={Gallery}
+              alt="gallery icon"
+            />
+          </div>
+          <div
+            onClick={submitPost}
+            className={
+              s.popupContentAddPost +
+              " " +
+              ((valuePostText !== "") & (valuePostTitle !== "") &&
+                s.popupContentAddPostSubmit)
+            }
+          >
+            <span>Post</span>
+          </div>
+        </div>
+      </div>
       <div className={s.news}>
-        {props.posts.map(post => (
+        {props.posts.map((post) => (
           <Post
             border={true}
             key={post.id}
@@ -45,7 +165,7 @@ const News = props => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
--- src/components/News/NewsContainer.jsx
+++ src/components/News/NewsContainer.jsx
@@ -1,12 +1,16 @@
-import React from "react"
-import { connect } from "react-redux"
-import { compose } from "redux"
-import { getTheme } from "./../../redux/AppReducer/app-selectors"
-import { getPosts } from "../../redux/PostsReducer/posts-selectors"
-import { deletePost, addLike } from "./../../redux/PostsReducer/posts-reducer"
-import News from "./News"
-import { getProfile } from "../../redux/ProfileReducer/profile-selectors"
-import Preloader from "./../../components/common/Preloader/Preloader"
+import React from "react";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { getTheme } from "./../../redux/AppReducer/app-selectors";
+import { getPosts } from "../../redux/PostsReducer/posts-selectors";
+import {
+  deletePost,
+  addLike,
+  addPostActionCreator,
+} from "./../../redux/PostsReducer/posts-reducer";
+import News from "./News";
+import { getProfile } from "../../redux/ProfileReducer/profile-selectors";
+import Preloader from "./../../components/common/Preloader/Preloader";
 
 class NewsContainer extends React.Component {
   render() {
@@ -20,19 +24,20 @@ class NewsContainer extends React.Component {
           addLike={this.props.addLike}
           strUrlPrev={this.props.strUrl}
           changeIndex={this.props.changeIndex}
+          addPostActionCreator={this.props.addPostActionCreator}
         />
       </>
-    )
+    );
   }
 }
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     posts: getPosts(state),
     theme: getTheme(state),
     profile: getProfile(state),
-  }
-}
+  };
+};
 
-export default compose(connect(mapStateToProps, { deletePost, addLike }))(
-  NewsContainer
-)
+export default compose(
+  connect(mapStateToProps, { deletePost, addLike, addPostActionCreator })
+)(NewsContainer);
